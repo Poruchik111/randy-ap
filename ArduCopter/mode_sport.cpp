f063035c1fcf80c50e205073d5b90cafc3df22df
max <poruchik111@gmail.com>
2025-03-25 19:28:22 +0200

    Chupakabra simple sport

--- ArduCopter/mode_sport.cpp
+++ ArduCopter/mode_sport.cpp
@@ -17,7 +17,7 @@ bool ModeSport::init(bool ignore_checks)
     pos_control->set_max_speed_accel_z(-get_pilot_speed_dn(), g.pilot_speed_up, g.pilot_accel_z);
     pos_control->set_correction_speed_accel_z(-get_pilot_speed_dn(), g.pilot_speed_up, g.pilot_accel_z);
 
-    SRV_Channels::set_output_pwm(SRV_Channel::k_motor5, 1100);
+    SRV_Channels::set_output_pwm(SRV_Channel::k_motor5, 1000);
     return true;
 }
 
@@ -100,13 +100,13 @@ void ModeSport::run()
     // limit front Pitch to -10 deg if max Pitch 30 deg and throttle high
     if (target_pitch < -100){
         // control Motors 5,6 over pitch stick and Throttle
-        uint16_t kthrtl = constrain_uint16((motors->get_throttle_out() - 0.2) *10, 0.01, 1.0);
+        //uint16_t kthrtl = constrain_uint16((motors->get_throttle_out() - 0.2) *10, 0.01, 1.0);
 
-        uint16_t chupamotors = (-target_pitch) / copter.aparm.angle_max * 800 * kthrtl + 1100;
+        uint16_t chupamotors = -target_pitch / copter.aparm.angle_max * 800 + 1000;
         target_pitch *= 0.33;
         SRV_Channels::set_output_pwm(SRV_Channel::k_motor5, chupamotors);
     }else{
-        SRV_Channels::set_output_pwm(SRV_Channel::k_motor5, 1100);
+        SRV_Channels::set_output_pwm(SRV_Channel::k_motor5, 1000);
     }   
 
     // call attitude controller
